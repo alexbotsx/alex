ff358a2125d37c6bc9a7c9977cbcbd8037ba12b9
Rengel777 <socio.44444@gmail.com>
2024-09-22 20:51:19 -0600

    Update main-creator.js

--- plugins/main-creator.js
+++ plugins/main-creator.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, usedPrefix, isOwner }) => {
 let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;RengelSix9⁩;;\nFN:RengelSix9⁩\nORG:RengelSix9\nTITLE:\nitem1.TEL;waid=972527282076:5218261275256\nitem1.X-ABLabel:RengelSix9\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:RengelSix9⁩\nEND:VCARD`
-await conn.sendMessage(m.chat, { contacts: { displayName: 'おBrayan.xyz⁩', contacts: [{ vcard }] }}, {quoted: m})
+await conn.sendMessage(m.chat, { contacts: { displayName: 'おRengel', contacts: [{ vcard }] }}, {quoted: m})
 }
 handler.help = ['owner']
 handler.tags = ['main']
