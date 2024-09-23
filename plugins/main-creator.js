0a56f83448de7ec011e3834cc2b9c0fde0321800
Rengel777 <socio.44444@gmail.com>
2024-09-22 20:52:26 -0600

    Update main-creator.js

--- plugins/main-creator.js
+++ plugins/main-creator.js
@@ -1,9 +1,16 @@
 let handler = async (m, { conn, usedPrefix, isOwner }) => {
-let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;RengelSix9⁩;;\nFN:RengelSix9⁩\nORG:RengelSix9\nTITLE:\nitem1.TEL;waid=972527282076:5218261275256\nitem1.X-ABLabel:RengelSix9\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:RengelSix9⁩\nEND:VCARD`
-await conn.sendMessage(m.chat, { contacts: { displayName: 'おRengel', contacts: [{ vcard }] }}, {quoted: m})
+  let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;RengelSix9;;\nFN:RengelSix9\nORG:RengelSix9\nTITLE:\nTEL;waid=972527282076:5218261275256\nX-ABLabel:RengelSix9\nEND:VCARD`
+  
+  await conn.sendMessage(m.chat, { 
+    contacts: { 
+      displayName: 'RengelSix9', 
+      contacts: [{ vcard }] 
+    }
+  }, { quoted: m })
 }
+
 handler.help = ['owner']
 handler.tags = ['main']
-handler.command = ['owner', 'creator', 'creador', 'dueño'] 
+handler.command = ['owner', 'creator', 'creador', 'dueño']
 
 export default handler
