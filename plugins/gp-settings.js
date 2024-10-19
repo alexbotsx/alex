1ad38417ed5422bd1ab703f706715c1312bb891e
RengelBott <git013354@gmail.com>
2024-10-18 20:56:15 -0600

    Update gp-settings.js

--- plugins/gp-settings.js
+++ plugins/gp-settings.js
@@ -1,21 +1,26 @@
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-    let isClose = { // Switch Case Like :v
-        'open': 'not_announcement',
-        'close': 'announcement',
-    }[(args[0] || '')]
+    let isClose = { 
+        'open': 'not_announcement',          // .group open
+        'close': 'announcement',             // .group close
+        'abrir': 'not_announcement',         // abrir grupo
+        'cerrar': 'announcement',            // cerrar grupo
+        'abrirgrupo': 'not_announcement',    // .grupo abrir
+        'cerrargrupo': 'announcement'        // .grupo cerrar
+    }[(args[0] || '').toLowerCase()]         // Permitir tanto minúsculas como mayúsculas
     if (isClose === undefined)
-
-    return m.reply(`
+        return m.reply(`
 🛡️ ${mssg.gpSetting}
 
 *▢ ${usedPrefix + command} close*
 *▢ ${usedPrefix + command} open*
+*▢ ${usedPrefix + command} cerrar*
+*▢ ${usedPrefix + command} abrir*
 `)
     await conn.groupSettingUpdate(m.chat, isClose)
 }
-handler.help = ['group']
+handler.help = ['group', 'grupo']
 handler.tags = ['group']
-handler.command = ['group', 'grupo'] 
+handler.command = ['group', 'grupo'] // Comandos para ".group" y ".grupo"
 handler.admin = true
 handler.botAdmin = true
 handler.group = true
