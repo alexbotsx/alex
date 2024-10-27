f5dc1e18e0f21e39f2cda6fdb951028c9252d759
RengelBott <git013354@gmail.com>
2024-10-27 00:55:29 -0600

    Update gp-link.js

--- plugins/gp-link.js
+++ plugins/gp-link.js
@@ -1,15 +1,24 @@
+let handler = async (m, { conn, groupMetadata }) => {
+    let link = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(m.chat);
+    
+    conn.sendButton(
+        m.chat,
+        `\n${mssg.linkGp} *${groupMetadata.subject}*\n\n${link}`,  // Mensaje que aparece
+        'Enlace del grupo',  // Texto adicional o pie del mensaje (opcional)
+        [
+            { buttonId: `.copylink`, buttonText: { displayText: 'Copiar enlace' }, type: 1 }
+        ]
+    );
 
-let handler = async (m, { conn, groupMetadata }) => { 
+    // Definir un comando para copiar automáticamente el enlace cuando el usuario presiona el botón
+    conn.copyLink = async (msg) => {
+        await conn.reply(msg.chat, '🔗 Enlace copiado: ' + link, msg);
+    };
+};
 
- let link = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(m.chat)
- conn.reply(m.chat, `\n${mssg.linkGp} *${groupMetadata.subject}*\n\n${link}`, m, {detectLink: true})
-//conn.sendHydrated(m.chat, `\n${mssg.linkGp} *${groupMetadata.subject}*\n\n${link}`, packname, null, 'https://www.whatsapp.com/otp/copy/https://chat.whatsapp.com/' + await conn.groupInviteCode(m.chat), 'Copiar', null, null, [ null ], m)
+// Handler para copiar el enlace
+handler.command = ['copylink'];
+handler.group = true;
+handler.botAdmin = true;
 
-}
-handler.help = ['link']
-handler.tags = ['group']
-handler.command = ['linkgroup', 'link'] 
-handler.group = true
-handler.botAdmin = true
-
-export default handler
+export default handler;
