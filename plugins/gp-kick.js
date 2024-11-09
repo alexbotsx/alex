6effce45d403f11e9fa9504505572a4040c4d486
RengelBott <git013354@gmail.com>
2024-11-09 10:12:35 -0600

    Update gp-kick.js

--- plugins/gp-kick.js
+++ plugins/gp-kick.js
@@ -1,20 +1,22 @@
+let handler = async (m, { conn, text, command }) => {
+    // Comprobamos si el mensaje menciona a un usuario o si responde a un mensaje
+    if (!m.mentionedJid[0] && !m.quoted) return m.reply(`✳️ ${mssg.useCmd}\n\n*${command}* @tag`);
+    let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender;
 
-let handler = async (m, { conn, usedPrefix, command }) => {
-	
-if (!m.mentionedJid[0] && !m.quoted) return m.reply(`✳️ ${mssg.useCmd}\n\n*${usedPrefix + command}* @tag`) 
-let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
-if (conn.user.jid.includes(user)) return m.reply(`✳️ No puedo hacer un auto kick`)
+    // Evitamos hacer un auto kick al bot
+    if (conn.user.jid.includes(user)) return m.reply(`✳️ No puedo hacer un auto kick`);
 
-await conn.groupParticipantsUpdate(m.chat, [user], 'remove')
-m.reply(`✅ ${mssg.kick}`) 
-
+    // Procedemos a expulsar al usuario mencionado
+    await conn.groupParticipantsUpdate(m.chat, [user], 'remove');
+    m.reply(`✅ ${mssg.kick}`);
 }
 
+// Permite que el comando funcione con o sin prefijo
 handler.help = ['kick @user']
 handler.tags = ['group']
-handler.command = ['kick', 'ivan', 'expulsar'] 
+handler.command = /^kick|ivan|expulsar$/i // Detección sin prefijo y con prefijo
 handler.admin = true
 handler.group = true
 handler.botAdmin = true
 
-export default handler
+export default handler;
